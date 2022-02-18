e260465b9ff3fd3d4495e2dc65339e650b85ac90
noelli <tobiah@noell.li>
2022-02-18 10:35:15 +0100

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+
+gulp.task('sass', function(cb) {
+    gulp
+        .src('assets/sass/*.scss')
+        .pipe(sass())
+        .pipe(
+            gulp.dest('assets/css/')
+        );
+    cb();
+});
+
+gulp.task(
+    'default',
+    gulp.series('sass', function(cb) {
+        gulp.watch('assets/sass/*.scss', gulp.series('sass'));
+        cb();
+    })
+);
